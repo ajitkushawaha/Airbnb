bbe000426d6cb49907b3dc08151f42257e165c25
Ajit Kushwaha <akk3311600@gmail.com>
2023-05-28 16:53:39 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './Components/Header';
+import HostProfile from './Components/HostProfile';
+import Maps from './Components/Maps';
+import Navbar from "./Components/Navbar";
+import Review from './Components/Review';
+import Photos from "./Components/Photos";
+import Footer from './Components/Footer';
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Photos/>
+      <Navbar/>
+      <HostProfile/>
+      <Review/>
+      <Maps/>
+      <Footer/>
+      <Footer/>
     </div>
   );
 }
